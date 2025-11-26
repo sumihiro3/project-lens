1d3c3c876c93029302f035d555e2dfe0ff45a1f1
sumihiro3 <sumihiro@gmail.com>
2025-11-26 10:26:40 +0900

    テーマカラー調整

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -11,6 +11,30 @@ export default defineNuxtPlugin((nuxtApp) => {
     directives,
     theme: {
       defaultTheme: 'system',
+      themes: {
+        light: {
+          dark: false,
+          colors: {
+            primary: '#2C9A7A',
+            secondary: '#F4F5F7',
+            accent: '#E83929',
+            surface: '#FFFFFF',
+            background: '#FFFFFF',
+            error: '#E83929',
+          },
+        },
+        dark: {
+          dark: true,
+          colors: {
+            primary: '#4DB699',
+            secondary: '#262626',
+            accent: '#FF6B6B',
+            surface: '#333333',
+            background: '#121212',
+            error: '#FF6B6B',
+          },
+        },
+      },
     },
   })
   nuxtApp.vueApp.use(vuetify)
