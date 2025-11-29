1f6fbb7ddb851d9f4dba825a9427893943f14694
sumihiro3 <sumihiro@gmail.com>
2025-11-29 10:24:12 +0900

    chore: ESLint と Prettier をセットアップ
    - ESLint、TypeScript ESLint、Prettier をインストール
    - .eslintrc.js、.prettierrc、.eslintignore、.prettierignore を追加
    - lint、lint:fix、format、format:check スクリプトを追加
    - src-tauri ディレクトリを除外

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  extends: [
+    '@nuxt/eslint-config',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  plugins: ['@typescript-eslint'],
+  rules: {
+    'vue/multi-word-component-names': 'off',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    'prettier/prettier': 'warn',
+  },
+}
