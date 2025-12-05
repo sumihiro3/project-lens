6f310a4a55f896a8b1ba2f6678b8cdc7bf569f9c
sumihiro3 <sumihiro@gmail.com>
2025-12-05 17:56:56 +0900

    feat: ナビゲーションドロワーをレスポンシブ対応し、モバイルでの開閉とナビゲーション時の自動クローズを実装

--- src/app.vue
+++ src/app.vue
@@ -2,20 +2,40 @@
   <v-app>
     <v-app-bar color="primary">
       <template v-slot:prepend>
+        <v-app-bar-nav-icon v-if="!mdAndUp" @click="drawer = !drawer" class="mr-2"></v-app-bar-nav-icon>
         <img :src="logoUrl" alt="ProjectLens" width="48" height="48" class="ml-2 mr-0" />
       </template>
       <v-app-bar-title class="font-weight-black ml-1">{{ $t('app.title') }}</v-app-bar-title>
       <template v-slot:append>
         <span v-if="lastSyncTime" class="text-caption mr-4">{{ $t('app.lastSynced', { time: lastSyncTime }) }}</span>
-        <v-btn icon="mdi-cog" to="/settings"></v-btn>
       </template>
     </v-app-bar>
 
-    <v-navigation-drawer expand-on-hover rail>
+    <v-navigation-drawer 
+      v-model="drawer"
+      :permanent="mdAndUp"
+      :rail="mdAndUp"
+      :expand-on-hover="mdAndUp"
+    >
       <v-list>
-        <v-list-item prepend-icon="mdi-view-dashboard" :title="$t('app.dashboard')" to="/"></v-list-item>
-        <v-list-item prepend-icon="mdi-format-list-bulleted" :title="$t('app.issueList')" to="/issues"></v-list-item>
-        <v-list-item prepend-icon="mdi-cog" :title="$t('app.settings')" to="/settings"></v-list-item>
+        <v-list-item 
+          prepend-icon="mdi-view-dashboard" 
+          :title="$t('app.dashboard')" 
+          to="/"
+          @click="onNavigate"
+        ></v-list-item>
+        <v-list-item 
+          prepend-icon="mdi-format-list-bulleted" 
+          :title="$t('app.issueList')" 
+          to="/issues"
+          @click="onNavigate"
+        ></v-list-item>
+        <v-list-item 
+          prepend-icon="mdi-cog" 
+          :title="$t('app.settings')" 
+          to="/settings"
+          @click="onNavigate"
+        ></v-list-item>
       </v-list>
     </v-navigation-drawer>
 
@@ -28,11 +48,22 @@
 <script setup lang="ts">
 import { ref, onMounted, onUnmounted } from 'vue'
 import { listen } from '@tauri-apps/api/event'
+import { useDisplay } from 'vuetify'
 import logoImage from '~/public/logo.png'
+
+const { mdAndUp } = useDisplay()
 
 const logoUrl = logoImage
 const lastSyncTime = ref('')
+const drawer = ref(true)
 let unlisten: (() => void) | null = null
+
+// モバイル画面でナビゲーション後にドロワーを閉じる
+const onNavigate = () => {
+  if (!mdAndUp.value) {
+    drawer.value = false
+  }
+}
 
 onMounted(async () => {
   unlisten = await listen<string>('refresh-issues', (event) => {
